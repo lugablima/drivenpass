ad703f560775f55749b784d7b766e77360257dc0
Lucas <lugablima@gmail.com>
2022-09-06 14:42:49 -0300

    chore: add eslint and prettier setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import "express-async-errors";
 import router from "./routes/index";
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
+
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,6 @@ app.use(cors(), json());
 app.use(router);
 app.use(errorHandlerMiddleware);
 
-const PORT = Number(process.env.PORT) | 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
